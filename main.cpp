9b668402475d52f005050771415a4d3000f43a58
Joo <werberr0@gmail.com>
2024-11-24 21:14:01 +0900

    modify login query, delete duplicated messages

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 PGconn* connectDB() {
-    const char* conninfo = "dbname=gms user=juuseong password=asdf5788";
+    const char* conninfo = "dbname=gms user=juuseong";
     PGconn* conn = PQconnectdb(conninfo);
 
     if (PQstatus(conn) != CONNECTION_OK) {
@@ -24,32 +24,24 @@ bool login(PGconn *conn, int userID, const std::string &role) {
         return false;
     }
     if (role == "admin" ) {
-        if (userID == 23571113) {
-            std::cout << "Login successful as " << role << "!" << std::endl;
-            return true;
-        }
-        else {
-            std::cout << "Login failed!" << std::endl;
-            return false;
-        }
+        if (userID == 23571113) return true;
+        else return false;
     }
+
     // role에 's'를 붙여서 테이블 이름을 결정
     std::string tableName = role + "s";  // 예: "trainer" -> "trainers"
 
     // 해당 테이블에서 role을 확인하는 쿼리
-    std::string query = "SELECT role FROM " + tableName + " WHERE ID = " + std::to_string(userID) + ";";
+    std::string query = "SELECT * FROM " + tableName + " WHERE ID = " + std::to_string(userID) + ";";
     
     // 쿼리 실행
     PGresult *res = PQexec(conn, query.c_str());
 
     // 쿼리 실행 결과 확인
     if (PQresultStatus(res) == PGRES_TUPLES_OK && PQntuples(res) > 0) {
-        std::cout << "Login successful as " << role << "!" << std::endl;
         PQclear(res);
         return true;
     }
-
-    std::cout << "Login failed!" << std::endl;
     return false;
 }
 
@@ -60,10 +52,11 @@ int main() {
     int userID; std::cout << "Enter ID: "; std::cin >> userID;
     std::string role; std::cout << "Enter role (admin, member, trainer, fdstaff): "; std::cin >> role;
 
-    if (login(conn, userID, role)) {
-        std::cout << "User logged in successfully!" << std::endl;
-    } else {
+    if (login(conn, userID, role)) std::cout << "Login successful as " << role << "!" << std::endl;
+    else {
         std::cout << "Login failed!" << std::endl;
+        PQfinish(conn);
+        return 1;
     }
 
     PQfinish(conn);
